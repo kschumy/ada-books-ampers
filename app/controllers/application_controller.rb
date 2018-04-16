15d01cc8b75a590c98490b2d90131da1500cc741
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-04-16 09:16:14 -0700

    added things

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+
   def find_user
     @user = User.find_by(id: session[:user_id])
   end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,15 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  #
+  #
+	# def self.remove_white_space_from_attr_list(attribute_list)
+	# 	attribute_list.each do |work_attribute|
+	# 		work_attribute.squish! if !work_attribute.nil?
+	# 	end
+	# end
+  #
+	# def nil_blank_values
+	# 	self.description = nil if !self.description.nil? && self.description.blank?
+	# 	self.creator = nil if !self.creator.nil? && self.creator.blank?
+	# end
 end
